d1324c0dcfc47a625fdf471a9e9a7ee91ad2c17a
Cherrio-LLC <cherrio.llc@gmail.com>
2021-02-26 10:31:12 +0000

    This is a project for a friend of mine who wants to manipute two lists using MVVM

--- app/src/main/java/com/cherrio/store/MainViewModel.kt
+++ app/src/main/java/com/cherrio/store/MainViewModel.kt
@@ -11,71 +11,65 @@ class MainViewModel : ViewModel() {
     var rv2Items = ArrayList<StoreItem>()
 
     fun setRV1Value(storeItem: StoreItem, isAdd: Boolean){
-        var totalQuantity = 0
-        if (isAdd){
-            rv2Items.forEach { // Adding up the quantity to know if the new item can be added
-                totalQuantity += it.quantity
+        when(incrementOrDecrementForBothRvs(storeItem, isAdd)){
+            0 -> listenerObserver.value = true
+            1 -> {
+                val store = StoreItem(storeItem.item, ++storeItem.quantity, storeItem.id)
+                rv1ObserveringRv2.value = store
             }
-            if (rv2Items.size != 10) { // check to see if the total items in the rv is not 10
-                if (totalQuantity == 10) {// check to see if the total quantity is not 10
-                    listenerObserver.value = true
-                } else {
-                    val store = StoreItem(storeItem.item, ++storeItem.quantity, storeItem.id)
-                    rv1ObserveringRv2.value = store
-                }
-            }else{
-                listenerObserver.value = true
+            2 -> {
+                val store = StoreItem(storeItem.item, --storeItem.quantity, storeItem.id)
+                rv1ObserveringRv2.value = store
             }
-        }else{
-            val store = StoreItem(storeItem.item, --storeItem.quantity, storeItem.id)
-            rv1ObserveringRv2.value = store
         }
 
     }
     fun setRV2Value(storeItem: StoreItem, isAdd: Boolean){
-        //Check your requiredQuantity here too
-        println("rv2Items size: ${rv2Items.size}")
-        var totalQuantity = 0
-        if (isAdd){
-            rv2Items.forEach {
-                totalQuantity += it.quantity
+        when(incrementOrDecrementForBothRvs(storeItem, isAdd)){
+            0 -> listenerObserver.value = true
+            1 -> {
+                val store = StoreItem(storeItem.item, ++storeItem.quantity, storeItem.id)
+                rv2ObserveringRv1.value = store
             }
-            println("total quantity $totalQuantity")
-            if (rv2Items.size != 10) {
-                println("Total items is not 10!")
-                if (totalQuantity == 10) {
-                    println("TotalQuantity items is 10!")
-                    listenerObserver.value = true
-                } else {
-                    println("TotalQuantity items is not 10!")
-                    val store = StoreItem(storeItem.item, ++storeItem.quantity, storeItem.id)
-                    rv2ObserveringRv1.value = store
-                }
-            }else{
-                listenerObserver.value = true
+            2 -> {
+                val store = StoreItem(storeItem.item, --storeItem.quantity, storeItem.id)
+                rv2ObserveringRv1.value = store
             }
-        }else{
-            val store = StoreItem(storeItem.item, --storeItem.quantity, storeItem.id)
-            rv2ObserveringRv1.value = store
         }
 
     }
 
     fun setData(storeItem: StoreItem){
-        var totalQuantity = 0
-        rv2Items.forEach {
-            totalQuantity += it.quantity
-        }
-
-        if (totalQuantity != 10) {
+        if (!isTotalQuantityYet()) {
             rv1DataToRv2.value = storeItem
         }else{
             listenerObserver.value = true
         }
     }
     fun setRV2StoreItems(stores: ArrayList<StoreItem>){
-        println("Stores size: ${stores.size}")
         rv2Items = stores
+    }
+    fun incrementOrDecrementForBothRvs(storeItem: StoreItem, isAdd: Boolean) : Int{
+        return if (isAdd){
+            if (rv2Items.size != 10) {
+                if (isTotalQuantityYet()) {
+                    0
+                } else {
+                    1
+                }
+            }else{
+                0
+            }
+        }else{
+            2
+        }
+    }
+   private fun isTotalQuantityYet(): Boolean{
+        var totalQuantity = 0
+        rv2Items.forEach {
+            totalQuantity += it.quantity
+        }
+        return totalQuantity == 10
     }
 
 }
